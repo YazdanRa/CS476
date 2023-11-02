c4da3092008c137896d228d417516cec41479b96
Codyhagel16 <codyhagel16@gmail.com>
2023-11-02 13:45:01 -0700

    Login Buttons Routed

--- frontend/react-frontend/src/components/Home.js
+++ frontend/react-frontend/src/components/Home.js
@@ -1,15 +1,25 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './CSS/Home.css';
 
 function HomeForm() {
   return (
     <div className="login-container">
       <h1 className="website-title">WebsiteTitle</h1>
+      
       <input type="email" placeholder="Email" className="input-field"/>
       <input type="password" placeholder="Password" className="input-field"/>
+
       <a href="./Reset" className="forgot-password">Forgot your password?</a>
-      <button className="login-button">Login</button>
-      <button className="signup-button">Sign Up</button>
+
+      <Link to = "/dashboard">
+        <button className="login-button">Login</button>
+      </Link>
+
+      <Link to = "/signup">
+        <button className="signup-button">Sign Up</button>
+      </Link>
+
     </div>
   );
 }
