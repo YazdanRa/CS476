cbd8b2189d832fd66a24401222e77eee1300328a
Yazdan Ranjbar <yazdanra@icloud.com>
2023-11-10 03:45:59 -0600

    Updated URL for election APIs

--- backend/src/apps/polling/urls.py
+++ backend/src/apps/polling/urls.py
@@ -5,23 +5,15 @@ from . import views
 urlpatterns = [
     path("my", views.MyElectionsListView.as_view(), name="my-elections-list"),
     path("create", views.CreateElectionView.as_view(), name="create-election"),
-    path("votingHistory", views.VotingHistoryView.as_view(), name="vote-history"),
-    path(
-        "byAccessCode/<str:access_code>/",
-        include(
-            [
-                path("", views.ElectionDetailView.as_view(), name="election-detail"),
-                path("recordVote", views.RecordVoteView.as_view(), name="record-vote"),
-            ]
-        )
-    ),
+    path("votingHistory", views.VotingHistoryView.as_view(), name="my-voting-history"),
     path(
         "byID/<str:pk>/",
         include(
             [
-                path("", views.ElectionDetailView.as_view(), name="election-detail"),
+                path("", views.ElectionDetailView.as_view(), name="election-detail-id"),
                 path("recordVote", views.RecordVoteView.as_view(), name="record-vote"),
             ]
         )
     ),
+    path("byAccessCode/<str:access_code>/", views.ElectionDetailView.as_view(), name="election-detail-access-code"),
 ]
