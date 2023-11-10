5658cb8f90c4d9edc5c3508e18332ddb5e37a8fe
Yazdan Ranjbar <yazdanra@icloud.com>
2023-11-10 01:04:56 -0600

    retrieve elections via both access code and ID

--- backend/src/apps/polling/urls.py
+++ backend/src/apps/polling/urls.py
@@ -6,7 +6,16 @@ urlpatterns = [
     path("my", views.MyElectionsListView.as_view(), name="my-elections-list"),
     path("create", views.CreateElectionView.as_view(), name="create-election"),
     path(
-        "<str:access_code>/",
+        "byAccessCode/<str:access_code>/",
+        include(
+            [
+                path("", views.ElectionDetailView.as_view(), name="election-detail"),
+                path("recordVote", views.RecordVoteView.as_view(), name="record-vote"),
+            ]
+        )
+    ),
+    path(
+        "byID/<str:pk>/",
         include(
             [
                 path("", views.ElectionDetailView.as_view(), name="election-detail"),
