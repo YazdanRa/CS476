c8b532f30d708ffb03d6c1016e36fb25409081e5
Yazdan Ranjbar <yazdanra@icloud.com>
2023-11-12 05:10:57 -0600

    improved dashboard

--- frontend/react-frontend/src/pages/Dashboard/index.js
+++ frontend/react-frontend/src/pages/Dashboard/index.js
@@ -1,14 +1,42 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {useLocation, useNavigate} from "react-router-dom";
+import {Button, Input, notification, Space} from 'antd';
 
 import Menu from "../../components/Menu";
 
 import './styles.css';
+import {useFormik} from "formik";
+import {getElectionByAccessCode} from "../../services/election";
 
 
 function Dashboard() {
     const location = useLocation();
     const navigate = useNavigate();
+
+    const [access_code, setAccessCode] = useState('');
+
+    const formik = useFormik({
+        initialValues: {
+            access_code: access_code
+        },
+        enableReinitialize: true,
+        onSubmit: values => {
+            _accessElection(values.access_code);
+        },
+    });
+
+    const _accessElection = (access_code) => {
+        getElectionByAccessCode(access_code)
+            .then((result) => {
+                // TODO: navigate to the voting page of the election!
+                navigate(`/elections/${result.id}`);
+            })
+            .catch((error) => {
+                if (error.response.status === 425) {
+                    notification.error({message: error.response.data.message});
+                }
+            });
+    }
 
     return (
         <div className="dashboard">
@@ -20,10 +48,15 @@ function Dashboard() {
                 <div className="vote-container">
                     <h2>Vote</h2>
                     <p>Enter the survey access code to vote</p>
-                    <div className="input-container">
-                        <input type="text" placeholder="Survey Code"/>
-                        <button className="forward-button">&rarr;</button>
-                    </div>
+                    <Space.Compact className="input-container" style={{width: '100%'}}>
+                        <Input
+                            placeholder="Survey Code" maxLength={8}
+                            value={formik.values.access_code}
+                            onChange={(e) => setAccessCode(e.target.value.toUpperCase())}
+                        />
+                        <Button type="primary" className="forward-button"
+                                onClick={() => formik.submitForm()}>&rarr;</Button>
+                    </Space.Compact>
                 </div>
 
                 <div className="create-survey-container">
