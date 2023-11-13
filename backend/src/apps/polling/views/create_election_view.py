35130e57020edd665130a7fd575260e7a2dea17f
Baran Erfani <erfani.baran@gmail.com>
2023-11-13 17:25:28 -0600

    Added permission check

--- backend/src/apps/polling/views/create_election_view.py
+++ backend/src/apps/polling/views/create_election_view.py
@@ -1,4 +1,5 @@
 from rest_framework.generics import CreateAPIView
+from rest_framework.permissions import IsAuthenticated
 
 from apps.polling.serializers import ActionElectionSerializer
 
@@ -8,4 +9,5 @@ class CreateElectionView(CreateAPIView):
     Create election gateway
     """
 
+    permission_classes = [IsAuthenticated]
     serializer_class = ActionElectionSerializer
--- backend/src/apps/polling/views/modify_election_view.py
+++ backend/src/apps/polling/views/modify_election_view.py
@@ -1,4 +1,5 @@
 from rest_framework.generics import UpdateAPIView
+from rest_framework.permissions import IsAuthenticated
 
 from apps.polling.models import Election
 from apps.polling.serializers import ActionElectionSerializer
@@ -9,6 +10,7 @@ class ModifyElectionView(UpdateAPIView):
     Update election gateway
     """
 
+    permission_classes = [IsAuthenticated]
     serializer_class = ActionElectionSerializer
     queryset = Election.objects.all()
     lookup_field = "pk"
